0161c9c226e8dc40766c1733126d18df89ac4737
Dekard <dangkhoatanhao@gmail.com>
2023-07-24 15:44:45 +0700

    update add slugify

--- src/main/java/com/ufm/library/ApplicationConfig.java
+++ src/main/java/com/ufm/library/ApplicationConfig.java
@@ -5,15 +5,25 @@ import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import com.github.javafaker.Faker;
+import com.github.slugify.Slugify;
 
 @Configuration
 @EnableJpaAuditing
+@EnableSpringDataWebSupport
 public class ApplicationConfig {
     @Bean
     public Faker faker() {
         return new Faker(new Locale("vi"));
+    }
+
+    @Bean
+    public Slugify slugify() {
+        return Slugify.builder()
+                .locale(new Locale("vi"))
+                .build();
     }
 
 }
