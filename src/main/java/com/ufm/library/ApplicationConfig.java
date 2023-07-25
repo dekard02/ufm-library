b8655b5d039fa2258c3b4cbd11a11257e4ed43a0
Dekard <dangkhoatanhao@gmail.com>
2023-07-26 02:17:53 +0700

    update config

--- src/main/java/com/ufm/library/ApplicationConfig.java
+++ src/main/java/com/ufm/library/ApplicationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 import com.github.javafaker.Faker;
 import com.github.slugify.Slugify;
@@ -14,6 +15,12 @@ import com.github.slugify.Slugify;
 @EnableJpaAuditing
 @EnableSpringDataWebSupport
 public class ApplicationConfig {
+    // spring.data.web.pageable.one-indexed-parameters=true dosent work
+    @Bean
+    public PageableHandlerMethodArgumentResolverCustomizer pageableResolverCustomizer() {
+        return pageableResolver -> pageableResolver.setOneIndexedParameters(true);
+    }
+
     @Bean
     public Faker faker() {
         return new Faker(new Locale("vi"));
