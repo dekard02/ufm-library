0c29e1fd6f79e319135c3ea15ff699da0c0070cf
Dekard <dangkhoatanhao@gmail.com>
2023-08-07 21:06:10 +0700

    add security config

---
+++ src/main/java/com/ufm/library/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.ufm.library.security;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.ufm.library.security.provider.UsernamePasswordAuthenticationProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationEntryPoint authEntryPoint;
+    private final UsernamePasswordAuthenticationProvider basicAuthProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(config -> config.disable())
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(
+                        sessionConfig -> sessionConfig.sessionCreationPolicy(STATELESS))
+                .authorizeHttpRequests().anyRequest().permitAll()
+                .and()
+                .exceptionHandling(config -> config.authenticationEntryPoint(authEntryPoint));
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(basicAuthProvider);
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
