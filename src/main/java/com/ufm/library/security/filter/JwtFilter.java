d26fc573d75d10cc459b15b408ab9d28e2cfb5d2
Dekard <dangkhoatanhao@gmail.com>
2023-08-07 21:07:41 +0700

    add security filter

---
+++ src/main/java/com/ufm/library/security/filter/JwtFilter.java
@@ -0,0 +1,52 @@
+package com.ufm.library.security.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.ufm.library.security.UserService;
+import com.ufm.library.service.JwtService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var bearerToken = request.getHeader("Authorization");
+        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        var jwt = bearerToken.split(" ")[1];
+
+        var username = jwtService.getSubject(jwt);
+        UserDetails userDetail = userService.loadUserByUsername(username);
+        var authentication = new UsernamePasswordAuthenticationToken(
+                userDetail,
+                null,
+                userDetail.getAuthorities());
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+}
