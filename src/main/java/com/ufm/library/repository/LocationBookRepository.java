f67dfd9104ef7f1ced50274266f1afcf669b7652
Dekard <dangkhoatanhao@gmail.com>
2023-08-16 22:27:39 +0700

    update key type & add method

--- src/main/java/com/ufm/library/repository/LocationBookRepository.java
+++ src/main/java/com/ufm/library/repository/LocationBookRepository.java
@@ -1,11 +1,14 @@
 package com.ufm.library.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.ufm.library.entity.LocationBook;
+import com.ufm.library.entity.key.LocationBookKey;
 
-public interface LocationBookRepository extends JpaRepository<LocationBook, Long>,
-        QuerydslPredicateExecutor<LocationBook> {
-
+public interface LocationBookRepository extends JpaRepository<LocationBook, LocationBookKey>,
+                QuerydslPredicateExecutor<LocationBook> {
+        public List<LocationBook> findByBookId(Long bookId);
 }
