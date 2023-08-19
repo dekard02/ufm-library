4d09d32106803c68547d155f910744dea85369d7
Dekard <dangkhoatanhao@gmail.com>
2023-08-19 16:07:38 +0700

    update config redis

--- src/main/java/com/ufm/library/config/RedisConfig.java
+++ src/main/java/com/ufm/library/config/RedisConfig.java
@@ -1,27 +1,12 @@
 package com.ufm.library.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
-
-    @Value("${redis.host}")
-    private String redisHost;
-
-    @Value("${redis.port}")
-    private Integer redisPort;
-
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        var redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
-        return new LettuceConnectionFactory(redisConfig);
-    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
